3ad8ef491654548833f4e7d4ecd3e1f447651001
jpaidima <56632724+jpaidima@users.noreply.github.com>
2021-02-06 15:39:33 -0500

    Update sqlci_tips.sql

--- dba/sql/sqlci_tips.sql
+++ dba/sql/sqlci_tips.sql
@@ -5,3 +5,18 @@ set sqlformat csv
 
 -- To dispay data in json format 
 set sqlformat json 
+
+
+-- alias 
+alias oracntsess=select count(*) from v$session;
+
+oracntsess
+
+-- Last cmd 5 times with delay of 3 sec 
+repeat 5 3 
+
+-- For history 
+history 
+history usage 
+
+-- Can use up and down arrows for previous sql cmds 
