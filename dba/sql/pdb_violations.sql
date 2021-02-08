68cabc79f239c22154f6afbbe88b882b1967a4eb
jpaidima <56632724+jpaidima@users.noreply.github.com>
2021-02-08 14:57:05 -0500

    Create pdb_violations.sql

---
+++ dba/sql/pdb_violations.sql
@@ -0,0 +1,9 @@
+SET LINESIZE 200
+COLUMN time FORMAT A30
+COLUMN name FORMAT A30
+COLUMN cause FORMAT A30
+COLUMN message FORMAT A30
+SELECT time, name, cause, message
+FROM   pdb_plug_in_violations
+WHERE  status='PENDING'
+ORDER BY time;
