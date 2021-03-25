6ca72bf058df3520574815d450f14a6d1be54949
jpaidima <56632724+jpaidima@users.noreply.github.com>
2021-03-25 11:36:46 -0400

    Update dv.sql

--- dba/sql/dv.sql
+++ dba/sql/dv.sql
@@ -6,3 +6,8 @@ col value format A30
 select comp_name,status from dba_registry where comp_name in ('Oracle Database Vault','Oracle Label Security');
 
 select * from v$option where parameter in ('Oracle Label Security','Oracle Database Vault');
+
+
+col GRANTEE format A10
+col GRANTED_ROLE format A40
+select grantee , granted_role from dba_role_privs where granted_role='DV_PATCH_ADMIN' and grantee='SYS';
