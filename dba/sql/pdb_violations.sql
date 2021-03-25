27a8e28c0f3b75567da579f06474c8e40d371eba
jpaidima <56632724+jpaidima@users.noreply.github.com>
2021-03-25 11:35:02 -0400

    Update pdb_violations.sql

--- dba/sql/pdb_violations.sql
+++ dba/sql/pdb_violations.sql
@@ -2,7 +2,8 @@ SET LINESIZE 200
 COLUMN time FORMAT A30
 COLUMN name FORMAT A30
 COLUMN cause FORMAT A30
-COLUMN message FORMAT A30
+COLUMN message FORMAT A120
+set pagesize 100
 SELECT time, name, cause, message
 FROM   pdb_plug_in_violations
 WHERE  status='PENDING'
