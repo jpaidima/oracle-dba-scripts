cfd63b18da0160b7d935ad6027c69e9d6c273e26
jpaidima <56632724+jpaidima@users.noreply.github.com>
2021-05-04 17:32:48 +0530

    Create ora_list_user_sess.sql

---
+++ dba/sql/ora_list_user_sess.sql
@@ -0,0 +1,26 @@
+set echo off
+set feed off
+set veri off
+set pagesize 2000
+set linesize 200
+set escape on
+set serveroutput on
+col machine format a30
+col service_name format a40
+col service format a40
+col osuser format A20
+col program format A40
+
+prompt Total Sessions by Instance
+select count(*) "Sessions",inst_id "Instance"
+from gv\$session
+where username=upper('&1')
+group by inst_id
+order by inst_id;
+
+prompt
+prompt Session Details
+
+select inst_id,sid,osuser,machine,program, STATUS, LOGON_TIME, LAST_CALL_ET/60 as LAST_CALL_Min from gv$session w
+here
+username=upper('&1') order by inst_id;
